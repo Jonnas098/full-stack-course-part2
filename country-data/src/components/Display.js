0939c3b357c0a5829b6ca745d3f1e1aa2f1c7584
John Navarro <alessandro7833@gmail.com>
2023-05-26 14:54:26 -0600

    remove identations

--- country-data/src/components/Display.js
+++ country-data/src/components/Display.js
@@ -7,17 +7,17 @@ const Display = ({search, countriesToShow}) => {
       {search.length === 0
         ? <p>Write a country name to see the details</p>
         : countriesToShow.length > 10
-          ? <p>To many matches, specify another filter</p>
-          : countriesToShow.length === 1
-            ? countriesToShow.map((country, index) =>
-              <Country
-                key={index} name={country.name.common}
-                capital={country.capital}
-                populaton={country.population}
-                lang={country.languages}
-                flag={country.flag}
-              />)
-            : countriesToShow.map((country, index) => <h3 key={index}>{country.name.common}</h3>)}
+        ? <p>To many matches, specify another filter</p>
+        : countriesToShow.length === 1
+        ? countriesToShow.map((country, index) =>
+        <Country
+          key={index} name={country.name.common}
+          capital={country.capital}
+          populaton={country.population}
+          lang={country.languages}
+          flag={country.flag}
+        />)
+        : countriesToShow.map((country, index) => <h3 key={index}>{country.name.common}</h3>)}
     </div>
   )
 }
