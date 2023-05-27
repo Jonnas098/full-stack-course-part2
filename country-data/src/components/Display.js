6b99b6d5502b873e3aa198f729cbf971e8412444
John Navarro <alessandro7833@gmail.com>
2023-05-26 20:23:43 -0600

    moved the .map to an external variable

--- country-data/src/components/Display.js
+++ country-data/src/components/Display.js
@@ -2,6 +2,19 @@ import React from "react";
 import Country from "./Country";
 
 const Display = ({search, countriesToShow}) => {
+
+  const countryDetail = countriesToShow.map((country, index) =>
+    <Country key={index}
+     name={country.name.common}
+     capital={country.capital}
+     population={country.population}
+     lang={country.languages}
+     flag={country.flags.png}
+    />)
+
+  const singleCountry = countriesToShow.map((country, index) =>
+    <h3 key={index}>{country.name.common}</h3>)
+
   return(
     <div>
       {search.length === 0
@@ -9,15 +22,9 @@ const Display = ({search, countriesToShow}) => {
         : countriesToShow.length > 10
         ? <p>To many matches, specify another filter</p>
         : countriesToShow.length === 1
-        ? countriesToShow.map((country, index) =>
-        <Country
-          key={index} name={country.name.common}
-          capital={country.capital}
-          populaton={country.population}
-          lang={country.languages}
-          flag={country.flag}
-        />)
-        : countriesToShow.map((country, index) => <h3 key={index}>{country.name.common}</h3>)}
+        ? countryDetail
+        : singleCountry
+      }
     </div>
   )
 }
