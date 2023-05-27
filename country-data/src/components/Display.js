a38f933245fcaa63f6a606bcac7ac7ffd634c050
John Navarro <alessandro7833@gmail.com>
2023-05-27 16:21:31 -0600

    single country changes

--- country-data/src/components/Display.js
+++ country-data/src/components/Display.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Country from "./Country";
-
+import SingleCountry from "./SingleCountry";
 const Display = ({search, countriesToShow}) => {
 
   const countryDetail = countriesToShow.map((country, index) =>
@@ -10,24 +10,30 @@ const Display = ({search, countriesToShow}) => {
      population={country.population}
      lang={country.languages}
      flag={country.flags.png}
-    />)
+  />)
 
-  const singleCountry = countriesToShow.map((country, index) =>
-    <h3 key={index}>{country.name.common}</h3>)
+  const singleCountry = countriesToShow.map((country, index) =>(
+    <SingleCountry key={index}
+     country={country.name.common}
+     population={country.population}
+     capital={country.capital}
+     lang={country.languages}
+     flag={country.flags.png}
+    />)
+  )
 
   return(
     <div>
-      {search.length === 0
-        ? <p>Write a country name to see the details</p>
-        : countriesToShow.length > 10
-        ? <p>To many matches, specify another filter</p>
-        : countriesToShow.length === 1
-        ? countryDetail
-        : singleCountry
+      {search.length === 0 ?
+          <p>Write a country name to see the details</p>
+        : countriesToShow.length > 10 ?
+          <p>To many matches, specify another filter</p>
+        : countriesToShow.length === 1 ?
+          countryDetail
+        : <ul>{singleCountry}</ul>
       }
     </div>
   )
 }
 
 export default Display
-
