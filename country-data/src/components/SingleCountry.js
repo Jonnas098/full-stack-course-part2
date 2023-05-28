682bfaed290bfd9fb279eb1bd19331041e4acda8
John Navarro <alessandro7833@gmail.com>
2023-05-27 18:38:46 -0600

    dynamic button text and refactor changes

--- country-data/src/components/SingleCountry.js
+++ country-data/src/components/SingleCountry.js
@@ -3,31 +3,27 @@ import Country from "./Country";
 
 const SingleCountry = ({ country, population, alt, flag, capital, lang }) => {
   const [show, setShow] = useState(true)
-  const handleClick = () => {
-    setShow(!show)
-  }
+
   let languages = Object.values(lang);
 
-  console.log("Country name:",country, population)
+  const handleClick = () => setShow(!show)
 
   return(
-    <div className={"test-content"}>
+    <>
       <li>
-        <h3>{country}</h3>
-        <button onClick={()=>handleClick()}>Show details</button>
+        <h4>{country}</h4>
+        <button onClick={()=>handleClick()}>{show ? "Show Details" : "Hide Details"}</button>
       </li>
       {!show ?
-        <div className={"test-dos"}>
-          <Country
-            name={country}
-            capital={capital}
-            lang={languages}
-            population={population}
-            flag={flag}
-            alt={alt}/>
-        </div>
-        : <></>}
-    </div>
+        <Country
+          name={country}
+          capital={capital}
+          lang={languages}
+          population={population}
+          flag={flag}
+          alt={alt}/>
+        : null}
+    </>
   )
 }
 
