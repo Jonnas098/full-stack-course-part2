538e441e35000a7f6bd51b262bd38444faafb1b6
John Navarro <alessandro7833@gmail.com>
2023-05-30 10:33:46 -0600

    create the spinner component in a individual file

---
+++ notes_project/src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../app.css"
+const Spinner = () => {
+  return (
+    <div className={"scaling-dots"}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Spinner
