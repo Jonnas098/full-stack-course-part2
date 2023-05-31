ca6d776b2cf5921b3e8a290b3172bfa00fe5ec78
John Navarro <alessandro7833@gmail.com>
2023-05-30 23:49:14 -0600

    Delete axios import

--- notes_project/src/index.js
+++ notes_project/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import './index.css';
-import axios from "axios";
 
 // axios.get("http://localhost:3001/notes").then(res => {
 //   const notes = res.data
