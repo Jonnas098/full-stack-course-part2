e1d26f060aea66a249b744dcb889a5505d853ff1
John Navarro <alessandro7833@gmail.com>
2023-06-02 17:44:58 -0600

    change alert error for notification component

--- notes_project/src/App.js
+++ notes_project/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Spinner from "./components/Spinner"
 import Note from "./components/Note";
+import Footer from "./components/Footer";
 import noteService from "./services/notes"
+import Notification from "./components/ErrorComponent";
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+  const [notification, setNotificationMessage] = useState(null)
 
   const notesToShow = showAll
     ? notes
@@ -53,9 +56,12 @@ const App = () => {
         setNotes(notes.map(note=> note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `The note '${note.content}' was already deleted from server`
+        setNotificationMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(()=>{
+          setNotificationMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -63,6 +69,7 @@ const App = () => {
   return(
     <div>
       <h1>Notes</h1>
+      <Notification message={notification}/>
       <div>
         <button onClick={()=>setShowAll(!showAll)}>
           Show {showAll ? "Important" : "All"}
@@ -79,6 +86,7 @@ const App = () => {
         />
         <button type={"submit"}>Save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
