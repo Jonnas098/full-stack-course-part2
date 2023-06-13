28af984f71743242065f12b5c9286e87af105828
John Navarro <alessandro7833@gmail.com>
2023-06-12 18:33:35 -0600

    change the base url and delete de false note in the all request

--- notes_project/src/services/notes.js
+++ notes_project/src/services/notes.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 //const baseUrl = "http://localhost:3001/notes"
-const baseUrl = "http://localhost:3001/api/notes"
+const baseUrl = "/api/notes"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  // const nonExisting = {
-  //   id: 10000,
-  //   content: "This note is not saved to server",
-  //   date: new Date().toISOString(),
-  //   important: true
-  // }
-  //return request.then(response => response.data.concat(nonExisting))
   return request.then(response => response.data)
 }
 
@@ -24,4 +17,5 @@ const update = (id, newObj) => {
   return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {getAll, create, update}
