b7214bbdb718032999554e4ab838ec10cac540a4
John Navarro <alessandro7833@gmail.com>
2023-06-12 18:32:27 -0600

    non-important changes

--- notes-server/index.js
+++ notes-server/index.js
@@ -6,9 +6,12 @@
 // })
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+
 app.use(express.json())
+app.use(cors())
 
 let notes = [
   {
@@ -94,7 +97,7 @@ app.post('/api/notes/', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
